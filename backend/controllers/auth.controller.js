c10084b778945c2b2d61a5027c1e5c4c139d75ef
motalbeshabbir <motalebshabbir@gmail.com>
2026-03-11 14:06:22 +0600

    Add login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
@@ -33,4 +34,42 @@ const register = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { mobile, password } = req.body;
+    // mobile number check
+    const user = await User.findOne({ mobile });
+    if (!user) {
+      return res.status(404).json({ message: "Member not found" });
+    }
+    // match password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+    // Create JWT token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+        memberId: user.memberId,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" },
+    );
+    // sent toekn to response
+    res.status(200).json({
+      message: "Login successfull",
+      token,
+      user: {
+        fullName: user.fullName,
+        mobile: user.mobile,
+        role: user.role,
+        memberId: user.memberId,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+module.exports = { register, login };
